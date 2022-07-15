d0b9ce455946ba4599bd0ddb88305770ba01ef9d
Abhinav-Bhardwaj-09 <abhinavbhardwaj68@gmail.com>
2022-07-15 23:19:43 +0530

    Methods with 4 different Access Modifiers.

---
+++ Java Full Stack Development/Implementation of OOPS using Java with DS and Beyond/src/JavaDeepDiveBuildingBlocks/AssistedPractice/accessModifiers/AccessModifiers.java
@@ -0,0 +1,38 @@
+package JavaDeepDiveBuildingBlocks.AssistedPractice.accessModifiers;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: Abhinav Bhardwaj
+ * Date: 15/07/22
+ * Time: 22:15
+ */
+
+
+public class AccessModifiers {
+
+    public void accessModifiers1 () {
+        System.out.println("Method with 'Public' as Access Modifiers.");
+    }
+
+    void accessModifiers2 () {
+        System.out.println("Method with 'Default' as access Modifiers.");
+    }
+
+    private void accessModifiers3 () {
+        System.out.println("Method with 'Private' as Access Modifiers.");
+    }
+
+    protected void accessModifiers4 () {
+        System.out.println("Method with 'Protected' as Access Modifiers.");
+    }
+
+
+    public static void main(String[] args) {
+        AccessModifiers Obj = new AccessModifiers();
+        Obj.accessModifiers1();
+        Obj.accessModifiers2();
+        Obj.accessModifiers3();
+        Obj.accessModifiers4();
+    }
+
+}
