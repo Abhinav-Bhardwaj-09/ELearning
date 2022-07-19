a58bf85563f8a7f473596374d2daebdcca327d55
Abhinav-Bhardwaj-09 <abhinavbhardwaj68@gmail.com>
2022-07-19 15:28:52 +0530

    Implementation of constructors.

---
+++ Java Full Stack Development/Implementation of OOPS using Java with DS and Beyond/src/JavaDeepDiveBuildingBlocks/AssistedPractice/implementationOfConstructors/defaultConstructor/DefaultConstructor.java
@@ -0,0 +1,20 @@
+package JavaDeepDiveBuildingBlocks.AssistedPractice.implementationOfConstructors.defaultConstructor;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: Abhinav Bhardwaj
+ * Date: 19/07/22
+ * Time: 15:09
+ */
+
+
+public class DefaultConstructor {
+    int a;
+
+    public static void main(String[] args) {
+        System.out.println("\nCalling default constructor.");
+        DefaultConstructor DC = new DefaultConstructor();
+        System.out.println("Value of a = " + DC.a + " (initialised by default constructor)");
+
+    }
+}
---
+++ Java Full Stack Development/Implementation of OOPS using Java with DS and Beyond/src/JavaDeepDiveBuildingBlocks/AssistedPractice/implementationOfConstructors/userDefinedConstructor/ParameterizedAndNonParameterizedConstructor.java
@@ -0,0 +1,41 @@
+package JavaDeepDiveBuildingBlocks.AssistedPractice.implementationOfConstructors.userDefinedConstructor;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: Abhinav Bhardwaj
+ * Date: 19/07/22
+ * Time: 15:17
+ */
+
+
+public class ParameterizedAndNonParameterizedConstructor {
+    int a;
+    String b;
+
+    public ParameterizedAndNonParameterizedConstructor() {
+    }
+
+    // constructor Overloading
+    public ParameterizedAndNonParameterizedConstructor(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    void display() {
+        if(b==null) {
+            System.out.println("Object created by calling Non-Parameterized constructor.");
+        }
+        else {
+            System.out.println("Object created by calling Non-Parameterized constructor (Here, it is also a case of " + b + " as two constructors with different signature are present )");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ParameterizedAndNonParameterizedConstructor NPC = new ParameterizedAndNonParameterizedConstructor();
+        ParameterizedAndNonParameterizedConstructor PC = new ParameterizedAndNonParameterizedConstructor(1, "Constructor Overloading");
+
+        NPC.display();
+        PC.display();
+    }
+}
