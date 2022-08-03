790b41e7308a502b281c5fe1b2492a63b34af0ee
Abhinav-Bhardwaj-09 <abhinavbhardwaj68@gmail.com>
2022-08-03 19:23:15 +0530

    HTTPSession

---
+++ Java Full Stack Development/Become a Back-End Expert/AssistedPractice/HTTPSession/src/main/java/com/businesslogics/Logout.java
@@ -0,0 +1,36 @@
+package com.businesslogics;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Logout() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.invalidate();
+		
+		PrintWriter out = res.getWriter();
+		out.println("You are logged out successfully...");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
+
